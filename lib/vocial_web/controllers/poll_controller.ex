b5d1c3039636a60c36a16df170276509ce307d98
CodeAkio <siqueira_2004@hotmail.com>
2021-06-20 15:38:22 -0300

    List all polls

---
+++ lib/vocial_web/controllers/poll_controller.ex
@@ -0,0 +1,11 @@
+defmodule VocialWeb.PollController do
+  use VocialWeb, :controller
+
+  def index(conn, _params) do
+    polls = Vocial.Votes.list_polls()
+
+    conn
+    |> put_layout(:special)
+    |> render("index.html", polls: polls)
+  end
+end
