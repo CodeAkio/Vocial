abe02e2e3c6e51c1fccfca9cf63ac223dc7083f8
CodeAkio <siqueira_2004@hotmail.com>
2021-10-17 02:11:15 -0300

    CRUD votes

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :vocial, Vocial.Repo,
   username: "postgres",
-  password: "docker",
+  password: "postgres",
   database: "vocial_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
--- lib/vocial_web/controllers/poll_controller.ex
+++ lib/vocial_web/controllers/poll_controller.ex
@@ -4,8 +4,22 @@ defmodule VocialWeb.PollController do
   def index(conn, _params) do
     polls = Vocial.Votes.list_polls()
 
-    conn
-    |> put_layout(:special)
-    |> render("index.html", polls: polls)
+    render(conn, "index.html", polls: polls)
+  end
+
+  def new(conn, _params) do
+    poll = Vocial.Votes.new_poll()
+
+    render(conn, "new.html", poll: poll)
+  end
+
+  def create(conn, %{"poll" => poll_params, "options" => options}) do
+    split_options = String.split(options, ",")
+
+    with {:ok, _poll} <- Votes.create_poll_with_options(poll_params, split_options) do
+      conn
+      |> put_flash(:info, "Poll created successfully!")
+      |> redirect(to: Routes.poll_path(conn, :index))
+    end
   end
 end
